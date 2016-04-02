66370910e63f371ec159be7510d380d3a170903a
Philip <molo4nik11@gmail.com>
2016-04-02 19:35:49 +0300

    slider

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,12 +1,25 @@
 $(function () {
 
 	var color = "#ffcd34",
-		slides = conf().slides;
+		slides = conf().slides,
+		n = -1;
 	$(".image-panel").vegas({
 		walk : function () {
 			$(".vegas-timer-progress").css("backgroundColor", color);
+			$('.adverticement').addClass('out');
+			n = n + 1 >= adverticements.length ? 0 : n + 1;
+			console.log(n);
+			setTimeout(function () {
+				$('.adverticement').text(adverticements[n].text);
+				$('.adverticement').removeClass('out');
+			}, 300);
 		},
-		delay : 4000,
+		init : function () {
+			var el = $('<div/>').addClass('adverticement out').text(adverticements[0].text);
+			// console.log($('.image-panel-wrapper'));
+			$('.image-panel-wrapper').append(el);
+		},
+		delay : 2500,
 	    slides: slides
 	    // overlay: '/libs/vegas/dist/overlays/07.png'
 	});
@@ -17,3 +30,10 @@ $(function () {
 		smartSpeed : 250
   	});
 });
+
+var adverticements = [
+	{ text : 'Выезд замерщика бесплатно' },
+	{ text : 'Уникальный дизайн каждого изделения' },
+	{ text : 'Надежность гарантируется' },
+	{ text : 'Изготовление от 14 дней' }
+];
